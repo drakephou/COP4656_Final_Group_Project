d7a28c96c53c387d665170aa2861bdfefc6ef1e3
drakephou <phdrake@gmail.com>
2023-08-03 13:56:18 -0400

    Initial Commit

---
+++ app/src/main/java/com/zybooks/cop4656finalgroupproject/SubTask.java
@@ -0,0 +1,44 @@
+package com.zybooks.cop4656finalgroupproject;
+
+public class SubTask {
+    private int Id;
+    private int TaskId;
+    private String Description;
+    private StopWatch SubStopWatch;
+
+    public SubTask(int taskId)
+    {
+        TaskId = taskId;
+    }
+
+    public int getId() {return Id;}
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public int getTaskId() {
+        return TaskId;
+    }
+
+    public void setTaskId(int taskId) {
+        TaskId = taskId;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public StopWatch getSubStopWatch() {
+        return SubStopWatch;
+    }
+
+    public void setSubStopWatch(StopWatch subStopWatch) {
+        SubStopWatch = subStopWatch;
+    }
+
+}
---
+++ app/src/main/java/com/zybooks/cop4656finalgroupproject/Task.java
@@ -0,0 +1,22 @@
+package com.zybooks.cop4656finalgroupproject;
+
+public class Task {
+
+    //
+
+    private int Id;
+    private String Name;
+
+    public Task(String name)
+    {
+        Name = name;
+    }
+
+    public int getId() {return Id;}
+
+    public void setId(int id){Id = id;}
+
+    public String getName(){return Name;}
+
+    public void setName(String name){Name = name;}
+}
